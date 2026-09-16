a393aeff2e2d72e8ffa91852d963411e5fa2a48a
Park HyunGyu <qkrgusrb0123a@gmail.com>
2026-09-16 22:33:32 +0900

    feat: streamline desktop marketplace navigation

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -14,7 +14,7 @@ export default {
   expo: {
     name: 'RAMarket',
     slug: 'ramarket-mobile',
-    version: '1.1.4',
+    version: '1.1.5',
     platforms: ['ios', 'android', 'web'],
     orientation: 'portrait',
     icon: './assets/app-icon-ram.png',
