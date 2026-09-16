37bdfc37ee9640b2fe28b10ecc582e0bca871766
Park HyunGyu <qkrgusrb0123a@gmail.com>
2026-09-17 00:55:43 +0900

    feat: polish desktop marketplace header

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -14,7 +14,7 @@ export default {
   expo: {
     name: 'RAMarket',
     slug: 'ramarket-mobile',
-    version: '1.1.7',
+    version: '1.1.8',
     platforms: ['ios', 'android', 'web'],
     orientation: 'portrait',
     icon: './assets/app-icon-ram.png',
