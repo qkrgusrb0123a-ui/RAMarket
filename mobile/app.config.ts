1f598f5d60ba38cf11787595b1ba15c7661be7c7
Park HyunGyu <qkrgusrb0123a@gmail.com>
2026-09-16 17:23:18 +0900

    fix: polish responsive admin workflows

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -14,7 +14,7 @@ export default {
   expo: {
     name: 'RAMarket',
     slug: 'ramarket-mobile',
-    version: '1.1.1',
+    version: '1.1.2',
     platforms: ['ios', 'android', 'web'],
     orientation: 'portrait',
     icon: './assets/app-icon-ram.png',
